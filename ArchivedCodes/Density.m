0738ae7508e87564dda4b6de4053f819c7841907
yangyangfu <yangyang.fu@colorado.edu>
2018-08-31 23:52:59 -0600

    Clean SGA code for single objective optimization

--- ArchivedCodes/Density.m
+++
@@ -1,4 +0,0 @@
-function dens = Density(temp,hum,pres)
-    dens = pres/(1000*0.287042*temp*(1.+1.607858*hum));
-end
-
--- NSGA-II/fitsort.m
+++
@@ -1,7 +0,0 @@
-function [ output_args ] = infeasfitsort( opt,combinedpop )
-%UNTITLED Summary of this function goes here
-%   Detailed explanation goes here
-
-
-end
-
